2803c2b6200d6910c4d91f0724ac6924c7bcdeb7
Moulik rai <raimoulik@gmail.com>
2024-02-05 08:02:39 +0545

    update: add components in app.tsx

--- src/components/SignIn/SignIn.tsx
+++ src/components/SignIn/SignIn.tsx
@@ -69,7 +69,7 @@ export default function SignIn() {
                   type="button"
                   className="inline-flex w-full items-center justify-center rounded-md bg-black px-3.5 py-2.5 font-semibold leading-7 text-white hover:bg-black/80"
                 >
-                  Get started <ArrowRight className="ml-2" size={16} />
+                  Get started &rarr;
                 </button>
               </div>
             </div>
